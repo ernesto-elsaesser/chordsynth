c902edd9996340eda9fc09146ad2a6acaadf7b1a
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2026-02-27 21:36:18 +0100

    Replicate HiChord controls

--- synth.py
+++ synth.py
@@ -3,47 +3,43 @@ import math
 import ctypes
 from pysdl import *  # local PySDL2 folder
 
-# Audio parameters
+# TODO:
+# - show current key
+# - chord variation live
+
+
 SR = 16000
 BLOCKSIZE = 512
-MASTER_VOL = 0.2
+MASTER_VOL = 0.3
 
-# Envelope & Filter
 ATTACK = 0.02
 RELEASE = 0.05
 CUTOFF_HZ = 1200.0  # Low-pass filter cutoff
 
-PITCH_TO_MIDI = {
-    "C": 60,
-    "D": 62,
-    "E": 64,
-    "F": 65,
-    "G": 67,
-    "A": 69,
-    "B": 71,
+
+SCANCODE_TO_DEGREE = {
+    SDL_SCANCODE_1: 1,
+    SDL_SCANCODE_2: 2,
+    SDL_SCANCODE_3: 3,
+    SDL_SCANCODE_4: 4,
+    SDL_SCANCODE_5: 5,
+    SDL_SCANCODE_6: 6,
+    SDL_SCANCODE_7: 7,
+    # SDL_SCANCODE_RSHIFT: 1,
+    SDL_SCANCODE_SPACE: 1,
+    SDL_SCANCODE_RETURN: 2,
+    SDL_SCANCODE_B: 3,
+    SDL_SCANCODE_Y: 4,
+    SDL_SCANCODE_X: 5,
+    SDL_SCANCODE_A: 6,
+    SDL_SCANCODE_H: 7,
 }
 
-SCANCODE_TO_PITCH = {
-    SDL_SCANCODE_1: "C",
-    SDL_SCANCODE_2: "D",
-    SDL_SCANCODE_3: "E",
-    SDL_SCANCODE_4: "F",
-    SDL_SCANCODE_5: "G",
-    SDL_SCANCODE_6: "A",
-    SDL_SCANCODE_7: "B",
-    SDL_SCANCODE_RSHIFT: "C",
-    SDL_SCANCODE_SPACE: "C",
-    SDL_SCANCODE_RETURN: "D",
-    SDL_SCANCODE_B: "E",
-    SDL_SCANCODE_Y: "F",
-    SDL_SCANCODE_X: "G",
-    SDL_SCANCODE_A: "A",
-    SDL_SCANCODE_H: "B",
-}
+SCALE = [None, 0, 2, 4, 5, 7, 9, 11]  # major scale
 
 
-def midi_to_freq(m):
-    return 440.0 * 2 ** ((m - 69) / 12.0)
+def midi_to_freq(shift):
+    return 440.0 * 2 ** (shift / 12.0)
 
 
 class Synth:
@@ -53,15 +49,30 @@ class Synth:
         self.attack_samples = int(ATTACK * self.sr)
         self.release_samples = int(RELEASE * self.sr)
 
+        self.key = -9  # steps from A4, start at C4
+        self.mod = "maj"
+
         # Filter coefficient alpha
         self.alpha = (2.0 * math.pi * CUTOFF_HZ / self.sr) / (2.0 * math.pi * CUTOFF_HZ / self.sr + 1.0)
 
-    def note_on(self, pitch, intervals, shift):
-        if pitch in self.voices:
+    def note_on(self, degree):
+        if degree in self.voices:
             return
-        midi = PITCH_TO_MIDI[pitch] + shift
-        freqs = [midi_to_freq(midi + i) for i in intervals]
-        self.voices[pitch] = {
+        root = self.key + SCALE[degree]
+        chord = [0]
+        if self.mod == "sus4":
+            chord.append(5)
+        elif self.mod != "5":
+            is_maj = degree in {1, 4, 5}
+            if self.mod == "m":
+                is_maj = not is_maj
+            chord.append(4 if is_maj else 3)
+        is_dim = degree == 7
+        chord.append(6 if is_dim else 7)
+        if self.mod == "7":
+            chord.append(11)
+        freqs = [440.0 * 2 ** ((root + i) / 12.0) for i in chord]
+        self.voices[degree] = {
             'freqs': freqs,
             'phases': [0.0, 0.0, 0.0],
             'state': 'on',
@@ -71,8 +82,8 @@ class Synth:
             'lpf_state': 0.0  # Memory for the one-pole filter
         }
 
-    def note_off(self, pitch):
-        v = self.voices.get(pitch)
+    def note_off(self, degree):
+        v = self.voices.get(degree)
         if v and v['state'] == 'on':
             v['state'] = 'release'
             v['release_pos'] = 0
@@ -84,7 +95,7 @@ class Synth:
         out_buf = (ctypes.c_float * frames)()
         remove_list = []
 
-        for pitch, v in list(self.voices.items()):
+        for degree, v in self.voices.items():
             # --- Envelope Logic ---
             if v['state'] == 'on':
                 if v['env_pos'] < self.attack_samples:
@@ -122,7 +133,7 @@ class Synth:
                 v['release_pos'] += frames
                 v['env_level'] = env[n_release - 1] if n_release > 0 else 0.0
                 if v['release_pos'] >= self.release_samples:
-                    remove_list.append(pitch)
+                    remove_list.append(degree)
 
             # --- Sawtooth Synthesis ---
             voice_buf = [0.0] * frames
@@ -152,8 +163,8 @@ class Synth:
             for n in range(frames):
                 out_buf[n] += filtered_voice[n]
 
-        for pitch in remove_list:
-            del self.voices[pitch]
+        for degree in remove_list:
+            del self.voices[degree]
 
         # Scale and copy to SDL stream
         out_ptr = ctypes.cast(stream, ctypes.POINTER(ctypes.c_float))
@@ -177,9 +188,6 @@ window = SDL_CreateWindow(b"Synth", 0, 0, 640, 480, SDL_WINDOW_SHOWN)
 renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC)
 
 event = SDL_Event()
-jstick = None
-intervals = 0, 4, 7
-shift = 0
 running = True
 
 while running:
@@ -190,50 +198,38 @@ while running:
             sc = event.key.keysym.scancode
             if sc in (SDL_SCANCODE_POWER, SDL_SCANCODE_ESCAPE):
                 running = False
+            elif sc == SDL_SCANCODE_PAGEUP: # L1
+                synth.key += 12
+            elif sc == SDL_SCANCODE_PAGEDOWN: # R1
+                synth.key -= 12
+            elif sc == SDL_SCANCODE_F1: # L2
+                synth.key += 1
+            elif sc == SDL_SCANCODE_F2: # R2
+                synth.key -= 1
             elif sc == SDL_SCANCODE_UP:
-                shift = 12
+                synth.mod = "m"  # switch between major and minor
             elif sc == SDL_SCANCODE_DOWN:
-                shift = -12
+                synth.mod = "sus4"
             elif sc == SDL_SCANCODE_LEFT:
-                intervals = 0, 3, 7
+                synth.mod = "5"
             elif sc == SDL_SCANCODE_RIGHT:
-                intervals = 0, 7
+                synth.mod = "7"
             else:
-                pitch = SCANCODE_TO_PITCH.get(sc)
-                if pitch is not None:
-                    synth.note_on(pitch, intervals, shift)
+                degree = SCANCODE_TO_DEGREE.get(sc)
+                if degree is not None:
+                    synth.note_on(degree)
         elif event.type == SDL_KEYUP:
             sc = event.key.keysym.scancode
-            if sc in {SDL_SCANCODE_UP, SDL_SCANCODE_DOWN}:
-                shift = 0
-            if sc in {SDL_SCANCODE_LEFT, SDL_SCANCODE_RIGHT}:
-                intervals = 0, 4, 7
+            if sc in {SDL_SCANCODE_UP, SDL_SCANCODE_DOWN, SDL_SCANCODE_LEFT, SDL_SCANCODE_RIGHT}:
+                synth.mod = ""
             else:
-                pitch = SCANCODE_TO_PITCH.get(sc)
-                if pitch is not None:
-                    synth.note_off(pitch)
-        elif event.type == SDL_JOYDEVICEADDED:
-            jstick = SDL_JoystickOpen(event.jdevice.which)
-        elif event.type == SDL_JOYHATMOTION:
-            sc = event.jhat.value
-            if sc == SDL_HAT_UP:
-                intervals = 0, 4, 7
-            elif sc == SDL_HAT_LEFT:
-                intervals = 0, 7
-            elif sc == SDL_HAT_DOWN:
-                intervals = 0, 3, 7
+                degree = SCANCODE_TO_DEGREE.get(sc)
+                if degree is not None:
+                    synth.note_off(degree)
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255)
     SDL_RenderClear(renderer)
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255)
-    for offset in intervals:
-        rct = SDL_Rect(20 + offset * 40, 20, 20, 20)
-        SDL_RenderFillRect(renderer, rct)
-    for offset, pitch in enumerate(PITCH_TO_MIDI):
-        on = pitch in synth.voices
-        SDL_SetRenderDrawColor(renderer, 0, 255 if on else 0, 0 if on else 255, 255)
-        rct = SDL_Rect(20 + offset * 40, 50, 20, 20)
-        SDL_RenderFillRect(renderer, rct)
+    # TODO: print current chord via TTF module
     SDL_RenderPresent(renderer)
     
     SDL_Delay(10)
